7dcf2efe3c32c4f303c85d020994c72035ec449e
Robert DeBenedictis <robert@prx.org>
2015-01-16 17:24:56 -0500

    Turning AutoLoad On
    The play buttons stopped working when I turned off AutoLoad

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ $(function() {
         soundManager.createSound({
           id: sndmanID,
           url: audioURL,
-          autoLoad: false,
+          autoLoad: true,
           autoPlay: false,
           onload: function() {
             $('button', el).click(function(ev) {
