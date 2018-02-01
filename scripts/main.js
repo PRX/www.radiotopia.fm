86acfa3bf4f5e29f7cc9b29301a2f0c5336399c3
Chris Kalafarski <chris@farski.com>
2018-02-01 10:59:44 -0500

    Add some GA tracking to donate buttons and links

--- scripts/main.js
+++ scripts/main.js
@@ -14,18 +14,40 @@ var Radiotopia = {
     $(window).scroll(throttledScroll);
 
     $('#secondaryNavigation .folder').last().find('.subnav').append('<div><a href="#" data-behavior="open-modal" data-target="newsletter">Sign Up For Newsletter</a></div>');
-    $('#secondaryNavigation .folder:nth-of-type(2)').find('.subnav').append('<div><a href="#" data-behavior="open-modal" data-target="donate">Make a Donation</a></div>');
+    $('#secondaryNavigation .folder:nth-of-type(2)').find('.subnav').append('<div><a href="#" data-behavior="open-modal" data-target="donate" data-source="footer">Make a Donation</a></div>');
 
     if ($('.modal-wrapper.podcast-donate').length) {
-      $('#headerNav').find('#mainNavigation').append('<div class="external"><a href="#" data-behavior="open-modal" data-target="podcast-donate">Donate</a></div>');
+      $('#headerNav').find('#mainNavigation').append('<div class="external"><a href="#" data-behavior="open-modal" data-target="podcast-donate" data-source="nav">Donate</a></div>');
     } else {
-      $('#headerNav').find('#mainNavigation').append('<div class="external"><a href="#" data-behavior="open-modal" data-target="donate">Donate</a></div>');
+      $('#headerNav').find('#mainNavigation').append('<div class="external"><a href="#" data-behavior="open-modal" data-target="donate" data-source="nav">Donate</a></div>');
     }
 
     $('*[data-behavior="open-modal"]').on('click', function(e) {
       e.preventDefault();
       var target = $(this).attr('data-target');
       $('.modal-wrapper.'+target).fadeIn();
+
+      console.log();
+
+      var source = $(this).attr('data-source');
+      if (!source) {
+        source = "body";
+      }
+
+      console.log(source);
+
+      if (target === 'donate' || target === 'podcast-donate') {
+        var designation = $('.modal-wrapper.'+target).find('*[data-designation]').attr('data-designation');
+
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'Donate',
+          eventAction: 'Open modal from '+source,
+          eventLabel: designation,
+          eventValue: 1,
+          transport: 'beacon'
+        });
+      }
     });
 
     $('*[data-behavior="close-modal"]').on('click', function() {
